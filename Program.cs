9139db8dabffef565249880296e8bb638b90cc8c
Veselin Romić <omegavesko@gmail.com>
2018-11-01 20:21:13 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RSAOModerationBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
